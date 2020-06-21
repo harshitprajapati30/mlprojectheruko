b6b0c5b28c59a5ec32dd44fcf419ce85dc754ac1
harshitprajapati30 <57149732+harshitprajapati30@users.noreply.github.com>
2020-06-21 20:10:16 +0530

    Add files via upload

--- util.py
+++ util.py
@@ -32,13 +32,13 @@ def load_saved_artifacts():
     global __data_columns
     global __locations
 
-    with open("E:/P/server/artifacts/columns.json", 'r') as f:
+    with open("/static/columns.json", 'r') as f:
         __data_columns = json.load(f)['data_columns']
         __locations = __data_columns[3:]
 
     global __model
     if __model is None:
-        with open("E:/P/server/artifacts/model.pickle", 'rb') as f:
+        with open("model.pickle", 'rb') as f:
             __model = pickle.load(f)
     print("loading saved artifacts...done")
 
